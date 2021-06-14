6142cc8d41b81444e0c6a3cdd6df062b64fcb48b
minkek10 <ricca10adp@gmail.com>
2021-06-14 18:05:05 +0200

    beta language

--- js/index_home.js
+++ js/index_home.js
@@ -1,8 +1,11 @@
 checkDevice();
 
+var btnText = document.getElementById('btnTranslate');
 
 window.addEventListener('resize', checkDevice);
 document.getElementById("btnContener").addEventListener('click', alertJs);
+document.getElementById("btnTranslate").addEventListener('click', checkLanguage);
+
 
 function alertJs(){
     // Swal.fire(
@@ -24,9 +27,9 @@ function alertJs(){
                 confirmButtonColor: 'rgb(142,166,155)',
             })
     }
-
-    
 }
+
+
 
 
 
@@ -51,7 +54,6 @@ function checkDevice(){
         console.log(larg);
         var image = document.getElementById("screenModeImg");
         image.src = "../img/big_screen2.png";
-        
     }
 
     if (larg > 1650){
@@ -59,6 +61,89 @@ function checkDevice(){
         var image = document.getElementById("screenModeImg");
         image.src = "../img/veryBig_screen2.png";
     }
+}
+
+function checkLanguage(){
+    var flag = document.getElementById('btnTranslate').lastElementChild.attributes[2];
+    var btnText = document.getElementById('btnTranslate');
+    console.log(flag);
+
+    if (flag.nodeValue =="flag_uk"){
+        console.log("Change to uk");
+        btnText.innerHTML='<img src="./img/flag_fr.png" id="flag" tag="flag_fr">'
+        translate();
+        console.log("on revient dans Change to uk");
+    }
+    if (flag.nodeValue =="flag_fr"){
+        console.log("Change to fr");
+        btnText.innerHTML='<img src="./img/flag_uk.png" id="flag" tag="flag_uk">'
+        document.location.reload();
+
+
+    }
+}
+
+function removeTranslation(){
+    document.getElementById("aboutBtn").classList.remove("btnAbout");
+    //document.classList.remove("btnProject");
+}
+
+function translate(){
+
+    // banner
+    document.getElementsByClassName('btnAbout')[0].innerHTML="About me";
+    document.getElementById('btnProject').innerText="Project"
+
+    //hello
+    document.getElementById('hello').innerHTML=
+    `Hello and welcome everyone ! <br><br> My name is <br>
+    <div id="name"><b>Riccardo Carroyer</b></div> <img id="profileImg" src="./img/riccarde_tr.png" alt="">`;
+
+
+    //Section aboutMe
+    document.getElementsByClassName('aboutMe')[0].innerHTML="<z>|</z> About me <z>|</z>";
+    document.getElementById('txtAboutMe').innerHTML=`Passionate about computer tools from a young age, I quickly started my first websites with the FrontPage software of the Office suite, I was Webmaster of my football team at 11 years old!
+    As soon as I had the opportunity, I chose an IT orientation at school, where I learned the maintenance, assembly, installation, administration of Windows, Windows Server and some Linux distributions.
+    <br> <br> In the continuity of this learning, I successfully completed a bachelor's degree in IT Management, where I was taught some work experience with a few tools as well as a work methodology.
+    This experience taught me to work with databases, programming mechanisms but also the analysis and design of a project.
+    <br> <br> Following this graduate, I had the opportunity to be selected in a 6 month training at 40 hours a week as a "Full stack NodeJS" developer organized by Cefora-Téchnocité.
+    This training allowed me to further professionalize my perception and my working methods.
+    <br> <br> I am a patient and autonomous person but I do not hesitate to seek advice or a tip from an experienced person when I am facing difficulties or have questions.
+    <br> Teamwork is nice to me.`;
+
+    //Table left
+    document.getElementsByClassName('nameTable')[0].innerText="Name";
+    document.getElementsByClassName('birthTable')[0].innerText="Born on";
+    document.getElementsByClassName('countryTable')[0].innerText="Country";
+    document.getElementsByClassName('nationalityTable')[0].innerText="Nationality";
+    document.getElementsByClassName('cityTable')[0].innerText="City";
+    document.getElementsByClassName('hobbiesTable')[0].innerText="Hobbies";
+    document.getElementsByClassName('telTable')[0].innerText="Telephone";
+    document.getElementsByClassName('diplomasTable')[0].innerText="Diplomas";
+    document.getElementsByClassName('mediaTable')[0].innerText="Medias";
+
+     //Table left
+    document.getElementsByClassName('birthTableAnswer')[0].innerText="23 january 1992";
+    document.getElementsByClassName('countryTableAnswer')[0].innerText="Belgium";
+    document.getElementsByClassName('nationalityTableAnswer')[0].innerText="Belgian";
+    document.getElementsByClassName('hobbiesTableAnswer')[0].innerHTML="- New technologies<br>- Football supporter <br>- Walking <br>- Retro Gaming <br>";
+    document.getElementsByClassName('diplomasTableAnswer')[0].innerHTML=`- Secondary school diploma as " IT Technician" <br>- IT Management Bachelor`;
+
+    //Section languages
+    document.getElementsByClassName("languagesSection")[0].innerHTML="<z>|</z> The languages ​​used <z>|</z>";
+
+    //Section soft
+    document.getElementsByClassName("softwaresSection")[0].innerHTML="<z>|</z> The software used <z>|</z>";
+
+    //Section DB
+    document.getElementsByClassName("bdSection")[0].innerHTML="<z>|</z> The DB engines used <z>|</z>";
+
+    //Section Contact
+    document.getElementsByClassName("contactSection")[0].innerHTML="<z>|</z> Contact me <z>|</z>";
+    document.getElementsByClassName("contactMe")[0].innerText=
+    `Don't hesitate to contact me if my profile interests you or if you have any questions,
+    I would answer them with the greatest pleasure!` 
+    document.getElementsByClassName("btnContact")[0].innerText="Contact me"
 }
 
 
